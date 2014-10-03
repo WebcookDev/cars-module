e726431587b658bcc0a20b12212ebb00280addef
Jakub Šanda <sanda.jakubhtc@gmail.com>
2014-10-03 11:40:05 +0200

    create media entity

---
+++ src/Entity/Media.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * This file is part of the Cars module for webcms2.
+ * Copyright (c) @see LICENSE
+ */
+
+namespace WebCMS\CarsModule\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="cars_media")
+ */
+class Media extends \WebCMS\Entity\Entity
+{
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Car") 
+     */
+    private $car;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $fromImport;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $foto;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $main;
+
+
+    /**
+     * Gets the value of name.
+     *
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the value of name.
+     *
+     * @param mixed $name the name
+     *
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of car.
+     *
+     * @return mixed
+     */
+    public function getCar()
+    {
+        return $this->car;
+    }
+
+    /**
+     * Sets the value of car.
+     *
+     * @param mixed $car the car
+     *
+     * @return self
+     */
+    public function setCar($car)
+    {
+        $this->car = $car;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of from import.
+     *
+     * @return mixed
+     */
+    public function getFromImport()
+    {
+        return $this->fromImport;
+    }
+
+    /**
+     * Sets the value of from import.
+     *
+     * @param mixed $fromImport the from import
+     *
+     * @return self
+     */
+    public function setFromImport($fromImport)
+    {
+        $this->fromImport = $fromImport;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of foto.
+     *
+     * @return mixed
+     */
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    /**
+     * Sets the value of foto.
+     *
+     * @param mixed $foto the foto
+     *
+     * @return self
+     */
+    public function setFoto($foto)
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of main.
+     *
+     * @return mixed
+     */
+    public function getMain()
+    {
+        return $this->main;
+    }
+
+    /**
+     * Sets the value of main.
+     *
+     * @param mixed $main the main
+     *
+     * @return self
+     */
+    public function setMain($main)
+    {
+        $this->main = $main;
+
+        return $this;
+    }
+}
